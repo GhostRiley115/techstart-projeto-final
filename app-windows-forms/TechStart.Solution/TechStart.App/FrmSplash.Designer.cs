1495065fc112f1aab589026358fc545bcf205c0e
GhostRiley115 <clayton.brito115@gmail.com>
2025-11-18 15:25:55 -0300

    Add splash screen com GIF da TechStart e tela de login básica

---
+++ app-windows-forms/TechStart.Solution/TechStart.App/FrmSplash.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace TechStart.App
+{
+    partial class FrmSplash
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.picSplash = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picSplash)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picSplash
+            // 
+            this.picSplash.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picSplash.Image = global::TechStart.App.Properties.Resources.Iniciando_ambiente_TechStart___;
+            this.picSplash.Location = new System.Drawing.Point(0, 0);
+            this.picSplash.Name = "picSplash";
+            this.picSplash.Size = new System.Drawing.Size(800, 450);
+            this.picSplash.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picSplash.TabIndex = 0;
+            this.picSplash.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 4000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // FrmSplash
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picSplash);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmSplash";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TechStart - Carregando...";
+            this.Load += new System.EventHandler(this.FrmSplash_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picSplash)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picSplash;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ app-windows-forms/TechStart.Solution/TechStart.App/FrmSplash.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TechStart.App
+{
+    public partial class FrmSplash : Form
+    {
+        public FrmSplash()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmSplash_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Para o timer (pra não ficar executando várias vezes)
+            timer1.Stop();
+
+            // Cria a tela de login
+            FrmLogin login = new FrmLogin();
+
+            // Quando a tela de login for fechada, fecha também a splash (e assim fecha a aplicação)
+            login.FormClosed += (s, args) => this.Close();
+
+            // Mostra a tela de login
+            login.Show();
+
+            // Esconde a splash (ela continua "existindo", mas invisível)
+            this.Hide();
+        }
+    }
+}
