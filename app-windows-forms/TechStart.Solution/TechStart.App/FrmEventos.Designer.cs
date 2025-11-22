2519a6da43c0876cb601a186891397fb873a23e9
GhostRiley115 <clayton.brito115@gmail.com>
2025-11-22 01:39:25 -0300

    Adicionando tela de criação de eventos com o crud

--- app-windows-forms/TechStart.Solution/TechStart.App/FrmEventos.Designer.cs
+++ app-windows-forms/TechStart.Solution/TechStart.App/FrmEventos.Designer.cs
@@ -28,20 +28,265 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmEventos));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lvEventos = new System.Windows.Forms.ListView();
+            this.grpDadosEvento = new System.Windows.Forms.GroupBox();
+            this.cmbTipo = new System.Windows.Forms.ComboBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.dtpDataEvento = new System.Windows.Forms.DateTimePicker();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.txtLocal = new System.Windows.Forms.TextBox();
+            this.txtNomeEvento = new System.Windows.Forms.TextBox();
+            this.lblDescricao = new System.Windows.Forms.Label();
+            this.lblTipo = new System.Windows.Forms.Label();
+            this.lblLocal = new System.Windows.Forms.Label();
+            this.lblDataEvento = new System.Windows.Forms.Label();
+            this.lblNomeEvento = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.grpDadosEvento.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.lvEventos);
+            this.panel1.Controls.Add(this.grpDadosEvento);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(694, 530);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // lvEventos
+            // 
+            this.lvEventos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(22)))), ((int)(((byte)(40)))));
+            this.lvEventos.ForeColor = System.Drawing.Color.White;
+            this.lvEventos.FullRowSelect = true;
+            this.lvEventos.GridLines = true;
+            this.lvEventos.HideSelection = false;
+            this.lvEventos.Location = new System.Drawing.Point(12, 269);
+            this.lvEventos.Name = "lvEventos";
+            this.lvEventos.Size = new System.Drawing.Size(670, 249);
+            this.lvEventos.TabIndex = 6;
+            this.lvEventos.UseCompatibleStateImageBehavior = false;
+            this.lvEventos.View = System.Windows.Forms.View.Details;
+            this.lvEventos.SelectedIndexChanged += new System.EventHandler(this.lvEventos_SelectedIndexChanged);
+            // 
+            // grpDadosEvento
+            // 
+            this.grpDadosEvento.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(22)))), ((int)(((byte)(40)))));
+            this.grpDadosEvento.Controls.Add(this.cmbTipo);
+            this.grpDadosEvento.Controls.Add(this.btnSalvar);
+            this.grpDadosEvento.Controls.Add(this.btnLimpar);
+            this.grpDadosEvento.Controls.Add(this.dtpDataEvento);
+            this.grpDadosEvento.Controls.Add(this.btnEditar);
+            this.grpDadosEvento.Controls.Add(this.btnExcluir);
+            this.grpDadosEvento.Controls.Add(this.txtDescricao);
+            this.grpDadosEvento.Controls.Add(this.txtLocal);
+            this.grpDadosEvento.Controls.Add(this.txtNomeEvento);
+            this.grpDadosEvento.Controls.Add(this.lblDescricao);
+            this.grpDadosEvento.Controls.Add(this.lblTipo);
+            this.grpDadosEvento.Controls.Add(this.lblLocal);
+            this.grpDadosEvento.Controls.Add(this.lblDataEvento);
+            this.grpDadosEvento.Controls.Add(this.lblNomeEvento);
+            this.grpDadosEvento.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.grpDadosEvento.Location = new System.Drawing.Point(12, 12);
+            this.grpDadosEvento.Name = "grpDadosEvento";
+            this.grpDadosEvento.Size = new System.Drawing.Size(670, 251);
+            this.grpDadosEvento.TabIndex = 0;
+            this.grpDadosEvento.TabStop = false;
+            this.grpDadosEvento.Text = "Dados do evento";
+            // 
+            // cmbTipo
+            // 
+            this.cmbTipo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(22)))), ((int)(((byte)(40)))));
+            this.cmbTipo.ForeColor = System.Drawing.Color.White;
+            this.cmbTipo.FormattingEnabled = true;
+            this.cmbTipo.Location = new System.Drawing.Point(141, 102);
+            this.cmbTipo.Name = "cmbTipo";
+            this.cmbTipo.Size = new System.Drawing.Size(407, 21);
+            this.cmbTipo.TabIndex = 9;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnSalvar.ForeColor = System.Drawing.Color.Black;
+            this.btnSalvar.Location = new System.Drawing.Point(6, 222);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 2;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnLimpar.ForeColor = System.Drawing.Color.Black;
+            this.btnLimpar.Location = new System.Drawing.Point(589, 222);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 5;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // dtpDataEvento
+            // 
+            this.dtpDataEvento.CalendarMonthBackground = System.Drawing.Color.White;
+            this.dtpDataEvento.Location = new System.Drawing.Point(141, 50);
+            this.dtpDataEvento.Name = "dtpDataEvento";
+            this.dtpDataEvento.Size = new System.Drawing.Size(407, 20);
+            this.dtpDataEvento.TabIndex = 8;
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnEditar.ForeColor = System.Drawing.Color.Black;
+            this.btnEditar.Location = new System.Drawing.Point(198, 222);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(75, 23);
+            this.btnEditar.TabIndex = 3;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnExcluir.ForeColor = System.Drawing.Color.Black;
+            this.btnExcluir.Location = new System.Drawing.Point(404, 222);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.TabIndex = 4;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(22)))), ((int)(((byte)(40)))));
+            this.txtDescricao.ForeColor = System.Drawing.Color.White;
+            this.txtDescricao.Location = new System.Drawing.Point(141, 129);
+            this.txtDescricao.Multiline = true;
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(407, 87);
+            this.txtDescricao.TabIndex = 7;
+            // 
+            // txtLocal
+            // 
+            this.txtLocal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(22)))), ((int)(((byte)(40)))));
+            this.txtLocal.ForeColor = System.Drawing.Color.White;
+            this.txtLocal.Location = new System.Drawing.Point(141, 76);
+            this.txtLocal.Name = "txtLocal";
+            this.txtLocal.Size = new System.Drawing.Size(407, 20);
+            this.txtLocal.TabIndex = 6;
+            // 
+            // txtNomeEvento
+            // 
+            this.txtNomeEvento.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(22)))), ((int)(((byte)(40)))));
+            this.txtNomeEvento.ForeColor = System.Drawing.Color.White;
+            this.txtNomeEvento.Location = new System.Drawing.Point(141, 24);
+            this.txtNomeEvento.Name = "txtNomeEvento";
+            this.txtNomeEvento.Size = new System.Drawing.Size(407, 20);
+            this.txtNomeEvento.TabIndex = 5;
+            // 
+            // lblDescricao
+            // 
+            this.lblDescricao.AutoSize = true;
+            this.lblDescricao.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescricao.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lblDescricao.Location = new System.Drawing.Point(11, 129);
+            this.lblDescricao.Name = "lblDescricao";
+            this.lblDescricao.Size = new System.Drawing.Size(68, 17);
+            this.lblDescricao.TabIndex = 4;
+            this.lblDescricao.Text = "Descrição:";
+            // 
+            // lblTipo
+            // 
+            this.lblTipo.AutoSize = true;
+            this.lblTipo.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTipo.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lblTipo.Location = new System.Drawing.Point(11, 106);
+            this.lblTipo.Name = "lblTipo";
+            this.lblTipo.Size = new System.Drawing.Size(37, 17);
+            this.lblTipo.TabIndex = 3;
+            this.lblTipo.Text = "Tipo:";
+            // 
+            // lblLocal
+            // 
+            this.lblLocal.AutoSize = true;
+            this.lblLocal.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLocal.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lblLocal.Location = new System.Drawing.Point(11, 79);
+            this.lblLocal.Name = "lblLocal";
+            this.lblLocal.Size = new System.Drawing.Size(41, 17);
+            this.lblLocal.TabIndex = 2;
+            this.lblLocal.Text = "Local:";
+            // 
+            // lblDataEvento
+            // 
+            this.lblDataEvento.AutoSize = true;
+            this.lblDataEvento.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDataEvento.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lblDataEvento.Location = new System.Drawing.Point(11, 50);
+            this.lblDataEvento.Name = "lblDataEvento";
+            this.lblDataEvento.Size = new System.Drawing.Size(39, 17);
+            this.lblDataEvento.TabIndex = 1;
+            this.lblDataEvento.Text = "Data:";
+            // 
+            // lblNomeEvento
+            // 
+            this.lblNomeEvento.AutoSize = true;
+            this.lblNomeEvento.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNomeEvento.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lblNomeEvento.Location = new System.Drawing.Point(11, 24);
+            this.lblNomeEvento.Name = "lblNomeEvento";
+            this.lblNomeEvento.Size = new System.Drawing.Size(114, 17);
+            this.lblNomeEvento.TabIndex = 0;
+            this.lblNomeEvento.Text = "Nome do evento:";
             // 
             // FrmEventos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(694, 530);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FrmEventos";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "FrmEventos";
+            this.Text = "Eventos TechStart";
+            this.Load += new System.EventHandler(this.FrmEventos_Load);
+            this.panel1.ResumeLayout(false);
+            this.grpDadosEvento.ResumeLayout(false);
+            this.grpDadosEvento.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox grpDadosEvento;
+        private System.Windows.Forms.Label lblDescricao;
+        private System.Windows.Forms.Label lblTipo;
+        private System.Windows.Forms.Label lblLocal;
+        private System.Windows.Forms.Label lblDataEvento;
+        private System.Windows.Forms.Label lblNomeEvento;
+        private System.Windows.Forms.ComboBox cmbTipo;
+        private System.Windows.Forms.DateTimePicker dtpDataEvento;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.TextBox txtLocal;
+        private System.Windows.Forms.TextBox txtNomeEvento;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.ListView lvEventos;
     }
 }
--- app-windows-forms/TechStart.Solution/TechStart.App/FrmEventos.cs
+++ app-windows-forms/TechStart.Solution/TechStart.App/FrmEventos.cs
@@ -1,20 +1,342 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TechStart.App
 {
     public partial class FrmEventos : Form
     {
+        private readonly string caminhoArquivo;
+        private List<Evento> listaEventos = new List<Evento>();
+        private int idEmEdicao = 0; // 0 = novo, >0 = editando esse ID
+
         public FrmEventos()
         {
             InitializeComponent();
+
+            caminhoArquivo = Path.Combine(
+                Application.StartupPath,
+                "dados",
+                "eventos.txt"
+            );
+        }
+
+        // Classe que representa um evento
+        public class Evento
+        {
+            public int Id { get; set; }
+            public string Nome { get; set; }
+            public DateTime Data { get; set; }
+            public string Local { get; set; }
+            public string Tipo { get; set; }
+            public string Descricao { get; set; }
+        }
+
+        // ====== EVENTOS DO FORM ======
+
+        private void FrmEventos_Load(object sender, EventArgs e)
+        {
+            ConfigurarListView();
+            PopularComboTipo();
+            GarantirArquivoEventos();
+            CarregarEventosDoArquivo();
+        }
+
+        // Seu gradiente do panel1
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+            Rectangle rect = new Rectangle(0, 0, panel1.Width, panel1.Height);
+
+            using (LinearGradientBrush brush = new LinearGradientBrush(
+                rect,
+                Color.FromArgb(47, 72, 122),  // TOPO
+                Color.FromArgb(5, 8, 22),     // BASE
+                LinearGradientMode.Vertical)) // de cima pra baixo
+            {
+                e.Graphics.FillRectangle(brush, rect);
+            }
+        }
+
+        // ====== CONFIGURAÇÕES INICIAIS ======
+
+        private void ConfigurarListView()
+        {
+            lvEventos.Columns.Clear();
+            lvEventos.View = View.Details;
+            lvEventos.FullRowSelect = true;
+            lvEventos.GridLines = true;
+
+            lvEventos.Columns.Add("ID", 50);
+            lvEventos.Columns.Add("Nome", 200);
+            lvEventos.Columns.Add("Data", 100);
+            lvEventos.Columns.Add("Local", 150);
+            lvEventos.Columns.Add("Tipo", 120);
+        }
+
+        private void PopularComboTipo()
+        {
+            cmbTipo.Items.Clear();
+            cmbTipo.Items.Add("Palestra");
+            cmbTipo.Items.Add("Workshop");
+            cmbTipo.Items.Add("Treinamento");
+            cmbTipo.Items.Add("Lançamento");
+            cmbTipo.Items.Add("Meetup");
+            cmbTipo.Items.Add("Outro");
+        }
+
+        private void GarantirArquivoEventos()
+        {
+            if (!File.Exists(caminhoArquivo))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(caminhoArquivo));
+                File.Create(caminhoArquivo).Close();
+            }
+        }
+
+        // ====== CARREGAR / SALVAR TXT ======
+
+        private void CarregarEventosDoArquivo()
+        {
+            listaEventos.Clear();
+            lvEventos.Items.Clear();
+
+            string[] linhas = File.ReadAllLines(caminhoArquivo);
+
+            foreach (string linha in linhas)
+            {
+                if (string.IsNullOrWhiteSpace(linha))
+                    continue;
+
+                string[] partes = linha.Split(';');
+                if (partes.Length < 6)
+                    continue;
+
+                if (!int.TryParse(partes[0], out int id))
+                    continue;
+
+                DateTime data;
+                if (!DateTime.TryParse(partes[2], out data))
+                    data = DateTime.Today;
+
+                Evento ev = new Evento
+                {
+                    Id = id,
+                    Nome = partes[1],
+                    Data = data,
+                    Local = partes[3],
+                    Tipo = partes[4],
+                    Descricao = partes[5]
+                };
+
+                listaEventos.Add(ev);
+            }
+
+            foreach (Evento ev in listaEventos)
+            {
+                ListViewItem item = new ListViewItem(ev.Id.ToString());
+                item.SubItems.Add(ev.Nome);
+                item.SubItems.Add(ev.Data.ToShortDateString());
+                item.SubItems.Add(ev.Local);
+                item.SubItems.Add(ev.Tipo);
+
+                lvEventos.Items.Add(item);
+            }
+
+            LimparCampos();
+        }
+
+        private void SalvarEventosNoArquivo()
+        {
+            using (StreamWriter sw = new StreamWriter(caminhoArquivo, false))
+            {
+                foreach (Evento ev in listaEventos)
+                {
+                    string linha = string.Join(";",
+                        ev.Id,
+                        ev.Nome,
+                        ev.Data.ToString("yyyy-MM-dd"),
+                        ev.Local,
+                        ev.Tipo,
+                        (ev.Descricao ?? string.Empty).Replace(";", ",")
+                    );
+
+                    sw.WriteLine(linha);
+                }
+            }
+        }
+
+        // ====== FUNÇÕES DE APOIO ======
+
+        private void LimparCampos()
+        {
+            txtNomeEvento.Clear();
+            txtLocal.Clear();
+            txtDescricao.Clear();
+            cmbTipo.SelectedIndex = -1;
+            dtpDataEvento.Value = DateTime.Today;
+
+            idEmEdicao = 0;
+            lvEventos.SelectedItems.Clear();
+        }
+
+        private int ObterProximoId()
+        {
+            if (listaEventos.Count == 0)
+                return 1;
+
+            return listaEventos.Max(e => e.Id) + 1;
+        }
+
+        // ====== EVENTOS DE BOTÃO (CRUD) ======
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            string nome = txtNomeEvento.Text.Trim();
+            string local = txtLocal.Text.Trim();
+            string tipo = cmbTipo.SelectedItem != null ? cmbTipo.SelectedItem.ToString() : "";
+            string descricao = txtDescricao.Text.Trim();
+            DateTime data = dtpDataEvento.Value.Date;
+
+            if (string.IsNullOrEmpty(nome))
+            {
+                MessageBox.Show("Informe o nome do evento.", "Atenção",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtNomeEvento.Focus();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(local))
+            {
+                MessageBox.Show("Informe o local do evento.", "Atenção",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtLocal.Focus();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(tipo))
+            {
+                MessageBox.Show("Selecione o tipo do evento.", "Atenção",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                cmbTipo.DroppedDown = true;
+                return;
+            }
+
+            if (idEmEdicao == 0)
+            {
+                // Novo evento
+                Evento novo = new Evento
+                {
+                    Id = ObterProximoId(),
+                    Nome = nome,
+                    Data = data,
+                    Local = local,
+                    Tipo = tipo,
+                    Descricao = descricao
+                };
+
+                listaEventos.Add(novo);
+            }
+            else
+            {
+                // Editando evento existente
+                Evento existente = listaEventos.FirstOrDefault(ev => ev.Id == idEmEdicao);
+                if (existente != null)
+                {
+                    existente.Nome = nome;
+                    existente.Data = data;
+                    existente.Local = local;
+                    existente.Tipo = tipo;
+                    existente.Descricao = descricao;
+                }
+            }
+
+            SalvarEventosNoArquivo();
+            CarregarEventosDoArquivo();
+
+            MessageBox.Show("Evento salvo com sucesso!", "Sucesso",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (lvEventos.SelectedItems.Count == 0 || idEmEdicao == 0)
+            {
+                MessageBox.Show(
+                    "Selecione um evento na lista para editar.",
+                    "Atenção",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning
+                );
+                return;
+            }
+
+            Evento ev = listaEventos.FirstOrDefault(x => x.Id == idEmEdicao);
+            if (ev != null)
+            {
+                txtNomeEvento.Text = ev.Nome;
+                dtpDataEvento.Value = ev.Data;
+                txtLocal.Text = ev.Local;
+                cmbTipo.SelectedItem = ev.Tipo;
+                txtDescricao.Text = ev.Descricao;
+
+                txtNomeEvento.Focus();
+            }
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            if (lvEventos.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Selecione um evento na lista para excluir.", "Atenção",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            ListViewItem item = lvEventos.SelectedItems[0];
+            int id = int.Parse(item.SubItems[0].Text);
+
+            DialogResult resp = MessageBox.Show(
+                "Deseja realmente excluir este evento?",
+                "Confirmação",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question
+            );
+
+            if (resp == DialogResult.No)
+                return;
+
+            Evento ev = listaEventos.FirstOrDefault(x => x.Id == id);
+            if (ev != null)
+            {
+                listaEventos.Remove(ev);
+                SalvarEventosNoArquivo();
+                CarregarEventosDoArquivo();
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            LimparCampos();
+        }
+
+        // ====== SELEÇÃO NO LISTVIEW ======
+
+        private void lvEventos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lvEventos.SelectedItems.Count == 0)
+            {
+                idEmEdicao = 0;
+                return;
+            }
+
+            ListViewItem item = lvEventos.SelectedItems[0];
+            idEmEdicao = int.Parse(item.SubItems[0].Text);
         }
     }
 }
